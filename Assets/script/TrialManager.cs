b22f1eecefe6f8ec56581830dfb01436ea5b58c9
iajbenho <cristorsonbenjai.ho@gmail.com>
2016-12-07 17:31:46 +0800

    uploaded iso

---
+++ Assets/script/TrialManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TrialManager : MonoBehaviour {
+
+	private int randomInt;
+
+	public Text randomText;
+	public GameObject startButton;
+
+	// Use this for initialization
+	void Start () {
+	
+		//kokokokookokokokokokokok
+		//debug debug debug debug debug debug
+	}
+
+
+	public void ButtonClick(int buttonNum)
+	{
+		if (buttonNum == 0) {
+			startButton.SetActive (false);
+			randomInt = Random.Range (0, 20);
+			randomText.text = randomInt.ToString ();
+			StartCoroutine (ResetAll ());
+		}
+	}
+
+	IEnumerator ResetAll()
+	{
+		yield return new WaitForSeconds (5);
+		randomText.text = "";
+		startButton.SetActive (true);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
